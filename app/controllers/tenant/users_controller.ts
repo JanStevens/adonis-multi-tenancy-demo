4c5604c961c5ca6cb46bbf1ee2829c917570eab6
Jan Stevens <jan@spencer.co>
2025-03-25 15:28:26 +0100

    chore: increase the amount of queries so more connections are used

--- app/controllers/tenant/users_controller.ts
+++ app/controllers/tenant/users_controller.ts
@@ -1,11 +1,34 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import User from '#models/tenant/user'
+import { TenantService } from '#services/tenant_service'
 
 export default class UsersController {
   async index({ request }: HttpContext) {
     const page = request.input('page', 1)
     const limit = request.input('limit', 10)
+    const users = await User.query().preload('country').paginate(page, limit)
 
-    return User.query().preload('country').paginate(page, limit)
+    await User.query().preload('country').paginate(page, limit)
+    await User.query().preload('country').paginate(page, limit)
+    await User.query().preload('country').paginate(page, limit)
+    await User.query().preload('country').paginate(page, limit)
+    await User.query().preload('country').paginate(page, limit)
+    await User.query().preload('country').paginate(page, limit)
+
+    Promise.all([
+      User.query().preload('country').paginate(page, limit),
+      User.query().preload('country').paginate(page, limit),
+      User.query().preload('country').paginate(page, limit),
+      User.query().preload('country').paginate(page, limit),
+      User.query().preload('country').paginate(page, limit),
+      User.query().preload('country').paginate(page, limit),
+      User.query().preload('country').paginate(page, limit),
+      User.query().preload('country').paginate(page, limit),
+    ])
+
+    return {
+      searchPath: await TenantService.searchPath(),
+      users,
+    }
   }
 }
