ee8625ecb455e427f0e3e71d6c0a92e07d7e26d6
Jan Stevens <jan@spencer.co>
2025-03-25 15:27:56 +0100

    fix: return sorted list of countries

--- app/controllers/central/countries_controller.ts
+++ app/controllers/central/countries_controller.ts
@@ -4,8 +4,8 @@ import type { HttpContext } from '@adonisjs/core/http'
 export default class CountriesController {
   async index({ request }: HttpContext) {
     const page = request.input('page', 1)
-    const limit = request.input('limit', 10)
+    const limit = request.input('limit', 25)
 
-    return Country.query().paginate(page, limit)
+    return Country.query().orderBy('iso').paginate(page, limit)
   }
 }
